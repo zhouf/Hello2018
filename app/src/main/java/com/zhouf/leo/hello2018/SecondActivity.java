e96078385573f1483ab08d2c253b5e06bbe56461
zhouf <zhouf_t@sohu.com>
2018-04-15 21:26:16 +0800

    fix score activity

--- app/src/main/java/com/zhouf/leo/hello2018/SecondActivity.java
+++ app/src/main/java/com/zhouf/leo/hello2018/SecondActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class SecondActivity extends AppCompatActivity {
 
     @Override
@@ -15,30 +17,44 @@ public class SecondActivity extends AppCompatActivity {
 
 
     public void btn1(View v) {
-//        if(v.getId()==R.id.btn1){
-//
-//        }else if(v.getId()==R.id.btn1b){
-//
-//        }
-
-        show(1);
+        if(v.getId()==R.id.btn1){
+            show(1);
+        }else if(v.getId()==R.id.btn1b){
+            showb(1);
+        }
     }
 
     public void btn2(View v) {
-        show(2);
+        if(v.getId()==R.id.btn2){
+            show(2);
+        }else{
+            showb(2);
+        }
     }
 
     public void btn3(View v) {
-        show(3);
+        if(v.getId()==R.id.btn3){
+            show(3);
+        }else{
+            showb(3);
+        }
     }
 
     public void btnReset(View v) {
         TextView out = (TextView)findViewById(R.id.score);
         out.setText("0");
+
+        ((TextView)findViewById(R.id.score2)).setText("0");
     }
 
     private void show(int i){
         TextView out = (TextView)findViewById(R.id.score);
+        String oldScore = (String) out.getText();
+        String newScore = String.valueOf(Integer.parseInt(oldScore) + i);
+        out.setText(newScore);
+    }
+    private void showb(int i){
+        TextView out = (TextView)findViewById(R.id.score2);
         String oldScore = (String) out.getText();
         String newScore = String.valueOf(Integer.parseInt(oldScore) + i);
         out.setText(newScore);
